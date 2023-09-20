1e0ce954429dd1d68d6209f812d3e0ced2354982
SpiderDan98 <danielkemna@t-online.de>
2023-09-20 16:29:27 +0200

    fix: country zipcode restriction phpcs

--- bundles/country-zipcode-restriction/src/FondOfKudu/Zed/CountryZipcodeRestriction/Persistence/CountryZipcodeRestrictionPersistenceFactory.php
+++ bundles/country-zipcode-restriction/src/FondOfKudu/Zed/CountryZipcodeRestriction/Persistence/CountryZipcodeRestrictionPersistenceFactory.php
@@ -7,6 +7,7 @@ use Spryker\Zed\Kernel\Persistence\AbstractPersistenceFactory;
 /**
  * @method \FondOfKudu\Zed\CountryZipcodeRestriction\Persistence\CountryZipcodeRestrictionQueryContainerInterface getQueryContainer()
  * @method \FondOfKudu\Zed\CountryZipcodeRestriction\Persistence\CountryZipcodeRestrictionRepositoryInterface getRepository()
+ * @method \FondOfKudu\Zed\CountryZipcodeRestriction\CountryZipcodeRestrictionConfig getConfig()
  */
 class CountryZipcodeRestrictionPersistenceFactory extends AbstractPersistenceFactory
 {
