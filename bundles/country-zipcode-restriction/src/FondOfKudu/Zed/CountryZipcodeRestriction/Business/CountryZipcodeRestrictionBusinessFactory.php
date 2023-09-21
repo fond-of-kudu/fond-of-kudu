4e7b7fa3744575952eaecfed659dfff12f01255e
SpiderDan98 <40319023+SpiderDan98@users.noreply.github.com>
2023-09-21 10:56:44 +0200

    fix: bundles phpcs (#60)

--- bundles/country-zipcode-restriction/src/FondOfKudu/Zed/CountryZipcodeRestriction/Business/CountryZipcodeRestrictionBusinessFactory.php
+++ bundles/country-zipcode-restriction/src/FondOfKudu/Zed/CountryZipcodeRestriction/Business/CountryZipcodeRestrictionBusinessFactory.php
@@ -4,6 +4,11 @@ namespace FondOfKudu\Zed\CountryZipcodeRestriction\Business;
 
 use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
 
+/**
+ * @method \FondOfKudu\Zed\CountryZipcodeRestriction\CountryZipcodeRestrictionConfig getConfig()
+ * @method \FondOfKudu\Zed\CountryZipcodeRestriction\Persistence\CountryZipcodeRestrictionQueryContainerInterface getQueryContainer()
+ * @method \FondOfKudu\Zed\CountryZipcodeRestriction\Persistence\CountryZipcodeRestrictionRepositoryInterface getRepository()
+ */
 class CountryZipcodeRestrictionBusinessFactory extends AbstractBusinessFactory
 {
 }
