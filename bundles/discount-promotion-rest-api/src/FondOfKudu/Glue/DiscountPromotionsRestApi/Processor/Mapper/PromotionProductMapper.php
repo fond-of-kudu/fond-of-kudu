89a89ddc1b9ba5f8a9735ae86b4c3ea3bf1c953c
Pascal Fischert <p-fischer@gmx.ne>
2024-01-30 14:29:21 +0100

    wip

--- bundles/discount-promotion-rest-api/src/FondOfKudu/Glue/DiscountPromotionsRestApi/Processor/Mapper/PromotionProductMapper.php
+++ bundles/discount-promotion-rest-api/src/FondOfKudu/Glue/DiscountPromotionsRestApi/Processor/Mapper/PromotionProductMapper.php
@@ -8,6 +8,41 @@ use Generated\Shared\Transfer\PromotedProductTransfer;
 class PromotionProductMapper implements PromotionProductMapperInterface
 {
     /**
+     * @var string
+     */
+    public const PRODUCT_ATTR_BRAND = 'brand';
+
+    /**
+     * @var string
+     */
+    public const PRODUCT_ATTR_MODEL = 'model';
+
+    /**
+     * @var string
+     */
+    public const PRODUCT_ATTR_MODEL_KEY = 'modelKey';
+
+    /**
+     * @var string
+     */
+    public const PRODUCT_ATTR_MODEL_SIZE = 'modelSize';
+
+    /**
+     * @var string
+     */
+    public const PRODUCT_ATTR_STYLE = 'style';
+
+    /**
+     * @var string
+     */
+    public const PRODUCT_ATTR_STYLE_KEY = 'styleKey';
+
+    /**
+     * @var string
+     */
+    public const PRODUCT_ATTR_TYPE = 'type';
+
+    /**
      * @param \Generated\Shared\Transfer\ProductViewTransfer $productViewTransfer
      * @param int $discountAmount
      *
@@ -19,6 +54,30 @@ class PromotionProductMapper implements PromotionProductMapperInterface
     ): PromotedProductTransfer {
         return (new PromotedProductTransfer())
             ->fromArray($productViewTransfer->toArray(), true)
-            ->setDiscountPrice($productViewTransfer->getPrice() - $discountAmount);
+            ->setDiscountPrice($productViewTransfer->getPrice() - $discountAmount)
+            ->setBrand($this->getProductViewTransferAttribute($productViewTransfer, static::PRODUCT_ATTR_BRAND))
+            ->setModel($this->getProductViewTransferAttribute($productViewTransfer, static::PRODUCT_ATTR_MODEL))
+            ->setModelKey($this->getProductViewTransferAttribute($productViewTransfer, static::PRODUCT_ATTR_MODEL_KEY))
+            ->setModelSize($this->getProductViewTransferAttribute($productViewTransfer, static::PRODUCT_ATTR_MODEL_SIZE))
+            ->setStyle($this->getProductViewTransferAttribute($productViewTransfer, static::PRODUCT_ATTR_STYLE))
+            ->setStyleKey($this->getProductViewTransferAttribute($productViewTransfer, static::PRODUCT_ATTR_STYLE_KEY))
+            ->setType($this->getProductViewTransferAttribute($productViewTransfer, static::PRODUCT_ATTR_TYPE));
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\ProductViewTransfer $productViewTransfer
+     * @param string $attribute
+     *
+     * @return string
+     */
+    protected function getProductViewTransferAttribute(
+        ProductViewTransfer $productViewTransfer,
+        string $attribute
+    ): string {
+        if (!isset($productViewTransfer->getAttributes()[$attribute])) {
+            return '';
+        }
+
+        return $productViewTransfer->getAttributes()[$attribute];
     }
 }
