ef2585a40c4a7c39cb6769050fa4a4bfd01cc68c
Pascal Fischert <p-fischer@gmx.ne>
2024-06-19 10:13:42 +0200

    remove deprecated stuff

--- bundles/customer-password-changed-at-connector/src/FondOfKudu/Zed/CustomerPasswordChangedAtConnector/Business/CustomerPasswordChangedAtConnectorFacade.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Zed\CustomerPasswordChangedAtConnector\Business;
-
-use Spryker\Zed\Kernel\Business\AbstractFacade;
-
-class CustomerPasswordChangedAtConnectorFacade extends AbstractFacade implements CustomerPasswordChangedAtConnectorFacadeInterface
-{
-}
--- bundles/customer-password-changed-at-connector/src/FondOfKudu/Zed/CustomerPasswordChangedAtConnector/Business/CustomerPasswordChangedAtConnectorFacadeInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Zed\CustomerPasswordChangedAtConnector\Business;
-
-interface CustomerPasswordChangedAtConnectorFacadeInterface
-{
-}
