7c720aaa1b78a69314600217f0850f8a868606e0
Chris E <christopher.egger@fondof.de>
2024-09-04 14:20:46 +0200

    fix extend and change method name

--- bundles/verified-customer/src/FondOfKudu/Glue/VerifiedCustomer/Communication/Plugin/GlueApplication/IsCustomerVerifiedValidatorPlugin.php
+++ bundles/verified-customer/src/FondOfKudu/Glue/VerifiedCustomer/Communication/Plugin/GlueApplication/IsCustomerVerifiedValidatorPlugin.php
@@ -5,12 +5,13 @@ namespace FondOfKudu\Glue\VerifiedCustomer\Communication\Plugin\GlueApplication;
 use Generated\Shared\Transfer\RestErrorCollectionTransfer;
 use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
 use Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\RestRequestValidatorPluginInterface;
+use Spryker\Glue\Kernel\AbstractPlugin;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @method \FondOfKudu\Glue\VerifiedCustomer\VerifiedCustomerFactory getFactory()
  */
-class IsCustomerVerifiedValidator implements RestRequestValidatorPluginInterface
+class IsCustomerVerifiedValidatorPlugin extends AbstractPlugin implements RestRequestValidatorPluginInterface
 {
  /**
   * @param \Symfony\Component\HttpFoundation\Request $httpRequest
